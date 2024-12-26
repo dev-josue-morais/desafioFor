465e7ab79d40adce0948b03e8b3327fb789e7527
JOSUE DE SOUZA MORAIS <josue28jrd@hotmail.com>
2024-12-26 13:51:08 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ button.addEventListener("click", () => {
     let i = 0
     while (i < contacts.length){
         if (contacts[i].name.toLowerCase() === searchName) {
-            result = contacts[i].name;
+            result = contacts[i];
             break;
         }
         i++;
